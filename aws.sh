d366b762ca8c9e520e562050e9b23badbcb146bc
rookie <50002350+rookie2435@users.noreply.github.com>
2023-08-22 15:30:31 +0800

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -30,6 +30,7 @@ echo "安装docker"
 echo "安装nginx"
   wget https://raw.githubusercontent.com/rookie2435/subscrible/master/embyUrl.tar.gz
   tar -zxvf embyUrl.tar.gz
-  cd emby
+  rm -rf embyUrl.tar.gz
+  cd embyUrl
   /usr/local/bin/docker-compose up -d
 

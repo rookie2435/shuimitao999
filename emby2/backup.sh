86d476a9286f5ddf9f5b9e0ec67beeecfb7dc740
luominghui <15196587184@163.com>
2023-08-22 15:25:45 +0800

    1

--- emby2/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rclone delete peach:backup/emby2.tar.gz
-cd /var/lib
-rm -rf /var/lib/emby/cache
-tar -czvf emby2.tar.gz --exclude=metadata  emby
-rclone copy -P emby2.tar.gz peach:backup
-rm -rf emby2.tar.gz
